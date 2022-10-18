4dec8ac7382c47575ee6a264f145307d62371478
santihernandezmonet <santi.hernandezm96@gmail.com>
2022-10-18 20:24:27 -0300

    dividiendo vista login y registrar. Y agregando JSON usuarios

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,38 @@
 const path = require("path");
+const fs = require('fs');
+
+const usersFilePath = path.join(__dirname, '../database/userBase.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const userController = {
     login: function (req,res){
+        const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
         res.render("login")
         //res.sendFile(path.resolve (__dirname, "../views/login.html"))
     },
     cart: function (req,res){
         res.render("cart")
         //res.sendFile(path.resolve (__dirname, "../views/cart.html"))
+    },
+    register: function(req,res){
+        res.render('register');
+    },
+    store: function(req,res){
+
+
+        let usuarioNuevo = {
+			id: (users[users.length-1].id)+1,
+			name: req.body.name,
+			surname: req.body.surname,
+			email: req.body.email,
+			home: req.body.home
+		}
+		users.push(usuarioNuevo);
+
+		fs.writeFileSync(usersFilePath,JSON.stringify(users,null," "));
+
+		res.redirect('/');
+
     }
 }
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,13 @@ const userController = require('../controllers/userController');
 const express = require('express');
 const router = express.Router();
 
-router.get("/login", userController.login)
+//LOGUEARSE
+router.get("/login", userController.login);
+
+//CREAR USUARIO
+router.get('/login/register', userController.register);
+router.post('/login/register', userController.store);
+
 
 router.get("/carrito", userController.cart)
 
