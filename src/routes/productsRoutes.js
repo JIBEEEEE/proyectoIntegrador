8d9091fbbaf0608fabf3d7ca29add9b9bcc134d0
fsaporiti <facundo.saporiti@hotmail.com>
2022-11-13 19:37:01 -0300

    Exp-Validator aplicado en vista Product Creat

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
+const { body } = require("express-validator")
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {       // request, archivo y callback que almacena archivo en destino
@@ -16,12 +17,33 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+const valido = [
+    body("name").notEmpty().withMessage("Debes escribir el nombre del producto"),
+    body("price").notEmpty().withMessage("Debes decidir un precio para el producto"),
+    body("discount").notEmpty().withMessage("Debes detallar el descuento"),
+    body("category").notEmpty().withMessage("Debes elegir una categoria"),  
+    body("description").notEmpty().withMessage("Por favor escribir una breve descripcion del producto"),
+    body("imageProduct").custom ((value, { req }) => {
+        let file = req.file;
+        let extensiones = [ ".jpg", ".png", ".jpeg", ".gif"]
+        if (!file){
+                throw new Error("Tienes que subir una imagen del producto")
+        } else {
+            let fileExtension = path.extname(file.originalname);
+            if (!extensiones.includes(fileExtension)) {
+                throw new Error(`Las extensiones de archivo permitidas son ${extensiones.join(", ")}`)
+            }
+        }
+        return true;
+    })
+]
+
 router.get("/", productsController.catalogo)
 
 router.get("/detail/:id", productsController.productDetail)
 
 router.get("/create", productsController.create)
-router.post('/create', upload.single("imageProduct"), productsController.store); 
+router.post('/create', upload.single("imageProduct"), valido, productsController.store); 
 
 router.get('/edit/:id', productsController.edit); 
 router.put('/edit/:id', upload.single("imageProductEdit"), productsController.update); 
